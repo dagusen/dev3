32a3aa1faeb67026e168a85c43117c6b73a72230
dagusen <louisaleksieb.dagusen@gmail.com>
2017-12-26 10:22:12 -0500

    finished models in positions

---
+++ src/positions/models.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.db.models.signals import pre_save
+
+from django.conf import settings
+
+from django.core.urlresolvers import reverse
+
+from partylists.models import PartyList
+
+from partylists.utils import unique_slug_generator
+
+User = settings.AUTH_USER_MODEL
+
+# Create your models here.
+
+class Position(models.Model):
+	user 				= models.ForeignKey(User)
+	partylist			= models.ForeignKey(PartyList)
+	position_name 		= models.CharField(max_length=120)
+	timestamp			= models.DateTimeField(auto_now_add=True)
+	updated				= models.DateTimeField(auto_now=True)
+	slug				= models.SlugField(null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
+	def get_absolute_url(self):
+		return reverse('position:detail', kwargs={'slug': self.slug})
+
+	class Meta:
+		ordering = ['-updated', '-timestamp']
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(rl_pre_save_receiver, sender=Position)
--- src/project/settings/base.py
+++ src/project/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'partylists',
+    'positions'
 ]
 
 MIDDLEWARE = [
