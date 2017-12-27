6e0e38fbe1868ae7a966ae509eb92fcc845c2dd5
dagusen <louisaleksieb.dagusen@gmail.com>
2017-12-27 10:12:04 -0500

    added button hide

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,1 +1,10 @@
-$(".carousel-inner .item:first-child").addClass("active");
+$(document).ready(function(){
+$("#show-button").click(function(){
+$("#hide-button").show()
+$("#show-button").hide()
+});
+$("#hide-button").click(function(){
+$("#show-button").show()
+$("#hide-button").hide()
+});
+});
